592f710d172bf09da9ea4466f9dceee14f8c9d75
Marianny <marianny.souzza@gmail.com>
2019-07-04 22:58:06 -0300

    Init project

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 10; i++) {
+            int numeroVez = i * 100000;
+            System.out.println("EU TE AMO " + numeroVez + " MILHÕES");
+        }
+
+    }
+}
